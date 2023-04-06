d56da7e51fcc5f9b54b3159bf8c2354b08f6c9df
Tim Coffman <234244+timcoffman@users.noreply.github.com>
2023-04-06 16:40:37 -0500

    establish a transaction when performing dao operations during @PostConstruct

--- src/main/java/edu/ohsu/cmp/ecp/sds/SupplementalDataStorePartitionInterceptor.java
+++ src/main/java/edu/ohsu/cmp/ecp/sds/SupplementalDataStorePartitionInterceptor.java
@@ -8,12 +8,18 @@ import ca.uhn.fhir.jpa.dao.data.IPartitionDao;
 import ca.uhn.fhir.jpa.entity.PartitionEntity;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
+
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 @Interceptor
 @Component
@@ -27,13 +33,33 @@ public class SupplementalDataStorePartitionInterceptor {
 	@Inject
 	IPartitionDao daoPartition;
 
+    @Inject
+    @Named("transactionManager")
+    protected PlatformTransactionManager txManager;
+    
+    private void doInTransaction( Runnable task ) {
+    	TransactionTemplate tmpl = new TransactionTemplate(txManager);
+    	
+        tmpl.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+            	task.run() ;
+            }
+        });
+    }
+    
 	@PostConstruct
-	public void estabshLocalPartition() {
+	public void establishLocalPartition() {
 		String localPartitionName = sdsProperties.getPartition().getLocalName();
 
-		if (!daoPartition.findForName(localPartitionName).isPresent()) {
-			daoPartition.save(newLocalPartitionEntity());
-		}
+		/* during @PostConstruct, the dao transaction must be established */
+		doInTransaction( () -> {
+			
+			if (!daoPartition.findForName(localPartitionName).isPresent()) {
+				daoPartition.save(newLocalPartitionEntity());
+			}
+			
+		}) ;
 
 	}
 
