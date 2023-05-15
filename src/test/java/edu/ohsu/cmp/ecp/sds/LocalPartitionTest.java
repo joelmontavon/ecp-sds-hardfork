a58604e42f98fab9a51639f06678d8a7f75dadba
Tim Coffman <234244+timcoffman@users.noreply.github.com>
2023-05-15 09:48:53 -0500

    unit test: LocalPartitionTest

---
+++ src/test/java/edu/ohsu/cmp/ecp/sds/LocalPartitionTest.java
@@ -0,0 +1,35 @@
+package edu.ohsu.cmp.ecp.sds;
+
+import org.hl7.fhir.instance.model.api.IIdType;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Questionnaire;
+import org.hl7.fhir.r4.model.QuestionnaireResponse;
+import org.hl7.fhir.r4.model.Reference;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+
+public class LocalPartitionTest extends BaseSuppplementalDataStoreTest {
+
+	@Test
+	void canStoreAndRetrieveResourceInLocalPartition() {
+		IGenericClient client = client() ;
+
+		Patient pat = new Patient();
+		IIdType patId = client.create().resource(pat).execute().getId();
+
+		Questionnaire questionnaire = new Questionnaire() ; 
+		IIdType questId = client.create().resource(questionnaire).execute().getId();
+
+		QuestionnaireResponse questionnaireResponse  = new QuestionnaireResponse() ;
+		questionnaireResponse.setSubject( new Reference(patId) ) ;
+		questionnaireResponse.setQuestionnaire( questId.getValue() ) ;
+		IIdType questRespId = client.create().resource(questionnaireResponse).execute().getId();
+
+		QuestionnaireResponse readQuestResp = client.read().resource(QuestionnaireResponse.class).withId(questRespId).execute();
+
+		Assertions.assertNotNull( readQuestResp );
+
+	}
+}
--- src/test/java/edu/ohsu/cmp/ecp/sds/TestMockSupplementalDataStorePartitioningConfig.java
+++ src/test/java/edu/ohsu/cmp/ecp/sds/TestMockSupplementalDataStorePartitioningConfig.java
@@ -2,11 +2,16 @@ package edu.ohsu.cmp.ecp.sds;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
+import ca.uhn.fhir.interceptor.model.RequestPartitionId;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.exceptions.InvalidRequestException;
 import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRule;
@@ -23,8 +28,15 @@ public class TestMockSupplementalDataStorePartitioningConfig  {
 		;
 	
 	private static final SupplementalDataStorePartitionInterceptor MOCK_PARTITION_INTERCEPTOR =
-		new TestMockSupplementalDataStorePartitionInterceptor( MOCK_PARTITION_NAME ) ;
-		;
+			new TestMockSupplementalDataStorePartitionInterceptor(MOCK_PARTITION_NAME);
+			;
+	
+	private static final SupplementalDataStorePartitionInterceptor DEFAULT_PARTITION_INTERCEPTOR =
+			new TestDefaultPartitionSupplementalDataStorePartitionInterceptor()
+			;
+		
+	@Inject
+	SupplementalDataStoreProperties sdsProperties;
 	
 	/*
 	 * permit read/write of any resources, regardless of authentication
@@ -36,12 +48,16 @@ public class TestMockSupplementalDataStorePartitioningConfig  {
 	}
 
 	/*
-	 * for all read/write operations, use the mock partition
+	 * for all read/write operations, use the default partition
 	 */
 	@Bean
 	@Primary
+	@ConditionalOnMissingBean(PartitionAwareTestConfig.class)
 	public SupplementalDataStorePartitionInterceptor mockPartitionInterceptor() {
-		return MOCK_PARTITION_INTERCEPTOR ;
+		// return MOCK_PARTITION_INTERCEPTOR ;
+		// return DEFAULT_PARTITION_INTERCEPTOR ;
+		final String localPartitionName = sdsProperties.getPartition().getLocalName();
+		return new TestMockSupplementalDataStorePartitionInterceptor( localPartitionName ) ;
 	}
 	
 	private static class TestMockSupplementalDataStoreAuthenticationInterceptor extends SupplementalDataStoreAuthorizationInterceptor {
@@ -50,6 +66,17 @@ public class TestMockSupplementalDataStorePartitioningConfig  {
 			IAuthRuleBuilder ruleBuilder = new RuleBuilder();
 			return ruleBuilder.allowAll("mock authentication interceptor").build() ;
 		}
+	}
+	
+	private static class TestDefaultPartitionSupplementalDataStorePartitionInterceptor extends SupplementalDataStorePartitionInterceptor {
+		
+		public TestDefaultPartitionSupplementalDataStorePartitionInterceptor() { }
+		
+		@Override
+		public RequestPartitionId partitionIdFromRequest(RequestDetails theRequestDetails) {
+			return RequestPartitionId.defaultPartition() ;
+		}
+		
 	}
 	
 	private static class TestMockSupplementalDataStorePartitionInterceptor extends SupplementalDataStorePartitionInterceptor {
