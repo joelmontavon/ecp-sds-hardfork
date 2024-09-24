540617f077fca32f72917360e13835f3b7e1eea9
Joel Montavon <joelmontavon@users.noreply.github.com>
2024-09-24 09:52:07 -0500

    Update introspect-proxy.py to read auth token and return payload

--- introspect-proxy/introspect-proxy.py
+++ introspect-proxy/introspect-proxy.py
@@ -1,22 +1,34 @@
-#!/usr/bin/env python3
-
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
-import logging
-import sys
+import json
+import jwt
 
+class JsonRequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        auth_header = self.headers.get('Authorization')
+        if auth_header and auth_header.startswith('Bearer '):
+            try:
+                token = auth_header.split(' ')[1]
+                payload = jwt.decode(token, options={"verify_signature": False})
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps(payload).encode())
+            except:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b'Unauthorized')
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Unauthorized')
 
-listen_port = int( sys.argv[1] )
-response_filename = sys.argv[2]
+def run_server():
+    server_address = ('', 8181)
+    httpd = HTTPServer(server_address, JsonRequestHandler)
+    print('Server running on port 8181...')
+    httpd.serve_forever()
 
-class IntrospectHTTPRequestHandler(BaseHTTPRequestHandler):
-  def do_POST( self ):
-    self.send_response( 200 )
-    self.send_header('Content-type','application/json')
-    self.end_headers()
-    with open( response_filename, 'rb' ) as response_file:
-        contents = response_file.read()
-        self.wfile.write( contents )
-
-with HTTPServer( ('', listen_port), IntrospectHTTPRequestHandler ) as server:
-    server.serve_forever()
+if __name__ == '__main__':
+    run_server()
