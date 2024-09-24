739b179e182679298633e69f7e6bcf2e9b1a7d92
Joel Montavon <joelmontavon@users.noreply.github.com>
2024-09-24 09:58:04 -0500

    Update introspect-proxy.py to decode and return payload from access token

--- introspect-proxy/introspect-proxy.py
+++ introspect-proxy/introspect-proxy.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import os
-import logging
-import sys
+import json
+import jwt
 
 
 listen_port = int( sys.argv[1] )
@@ -11,12 +10,25 @@ response_filename = sys.argv[2]
 
 class IntrospectHTTPRequestHandler(BaseHTTPRequestHandler):
   def do_POST( self ):
-    self.send_response( 200 )
-    self.send_header('Content-type','application/json')
-    self.end_headers()
-    with open( response_filename, 'rb' ) as response_file:
-        contents = response_file.read()
-        self.wfile.write( contents )
+        auth_header = self.headers.get('Authorization')
+        if auth_header and auth_header.startswith('Bearer '):
+            try:
+                token = auth_header.split(' ')[1]
+                payload = jwt.decode(token, options={"verify_signature": False})
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(json.dumps(payload).encode())
+            except:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                self.wfile.write(b'Unauthorized')
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Unauthorized')
 
 with HTTPServer( ('', listen_port), IntrospectHTTPRequestHandler ) as server:
     server.serve_forever()
